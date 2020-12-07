dffb596e7bef398180390420bdac3b21df0d431b
casperkristiansen <71253446+casperkristiansen@users.noreply.github.com>
2020-12-07 10:27:58 +0100

    ddskf

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -4,21 +4,3 @@
 
 
 
-//Sticky navbar start
-
-window.onscroll = function () { myFunction() };
-
-var navbar = document.getElementById("navbar");
-
-var sticky = navbar.offsetTop;
-
-function myFunction() {
-   if (window.pageYOffset >= sticky) {
-       navbar.classList.add("sticky")
-   } else {
-        navbar.classList.remove("sticky");
-    }
-}
-//Sticky navbar end
-
-
